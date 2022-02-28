38cb52c062d1541966eecbdad27c46d4f434da33
Rafael Estrada <ntaticat@gmail.com>
2022-02-28 15:32:45 -0600

    initial commit

---
+++ src/views/PruebaView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+onMounted(() => {
+  console.log("Hola Mundo");
+})
+
+</script>
+
+<template>
+  <div class="about">
+    <h1>Hola mundo desde esta página</h1>
+    <button @click="increment">Contador es: {{ count }}</button>
+  </div>
+</template>
+
+<style>
+</style>
